61c0164ea08ca31da7c62578b2f1725b58c43cb0
Adrian Galera <adrian.galera@ocado.com>
2019-11-19 15:55:12 +0100

    Making react router work by assigning error to index.html

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "frontend-bucket" {
 
   website {
     index_document = "index.html"
-    #error_document = "error.html"
+    error_document = "index.html"
   }
   cors_rule {
     allowed_headers = ["*"]
